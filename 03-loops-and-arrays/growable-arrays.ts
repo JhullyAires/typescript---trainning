cbd7e623ad31d62c49c281828766f401935b8f29
Jhully Aires <jhullyolires@gmail.com>
2024-02-23 15:18:51 -0300

    overview typescript

---
+++ 03-loops-and-arrays/growable-arrays.ts
@@ -0,0 +1,8 @@
+let sportsTwo: string[] = ["Golf", "Cricket", "Swimming"];
+
+sportsTwo.push("Futbol");
+sportsTwo.push("Baseball");
+
+for (let tempSport of sportsTwo) {
+    console.log(tempSport);
+}
---
+++ 03-loops-and-arrays/sports.ts
@@ -0,0 +1,9 @@
+let sportsOne: string[] = ["Golf", "Cricket", "Swimming"];
+
+for (let tempSport of sportsOne) {
+    if (tempSport == "Cricket") {
+        console.log(tempSport + " << My minus favorite!");
+    } else {
+        console.log(tempSport);
+    }
+}
